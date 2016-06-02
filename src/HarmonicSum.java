09788191bed06ccba9b19c459e466ed7063bc782
Vladyslav Yakymenko <vladyslav_yakymenko@epam.com>
2016-06-03 02:36:12 +0300

    Added HurmonicSum class new method

--- src/HarmonicSum.java
+++ src/HarmonicSum.java
@@ -3,16 +3,16 @@
  */
 public class HarmonicSum {
     public static void main(String[] args) {
-        double harmonic = 0;
-        int n = 0;
-        /*for (int n = 0; n <= 50000; n++){
-            n++;
+        int harmonic = 0;
+        int n;
+        for (n = 1; n <= 50000; n++){
+            //n++;
             harmonic = harmonic + 1/n;
-        }*/
-        do {
+        }
+        /*do {
             n++;
-            harmonic = harmonic + 1/n;
-        } while (n == 50000);
+            harmonic = harmonic + (1/n);
+        } while (n <= 50000);*/
         System.out.println(harmonic);
     }
 }
