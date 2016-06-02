fe22b493095f785d9f37ee8fe10c147557f04a52
Vladyslav_Yakymenko <chameleon91@yandex.ru>
2016-06-02 11:24:22 +0300

    Added CheckOddEven class

---
+++ src/CheckOddEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CheckOddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            if (number % 2 == 0) {
+                System.out.println("Number " + number + " is odd");
+            } else
+                System.out.println("Number " + number + " is even");
+        } else
+            System.out.println("Enter number with type integer");
+    }
+}
