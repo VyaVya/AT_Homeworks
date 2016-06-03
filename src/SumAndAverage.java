d7eeeaec2fc719cae3b95f715f2faa180b0ee46d
Vladyslav_Yakymenko <chameleon91@yandex.ru>
2016-06-03 12:10:00 +0300

    Average is added to SumAnd Average class

--- src/SumAndAverage.java
+++ src/SumAndAverage.java
@@ -1,16 +1,16 @@
-/**
- * Created by Vladyslav_Yakymenko on 6/2/2016.
- */
+
 public class SumAndAverage {
     public static void main(String[] args) {
         int sumOfNumbers = 0;
         int[] arrayOf100Items = new int[100];
-        for(int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             arrayOf100Items[i] = i;
         }
         for (int i = 0; i < arrayOf100Items.length; i++) {
             sumOfNumbers = sumOfNumbers + arrayOf100Items[i];
         }
-        System.out.println(sumOfNumbers);
+        System.out.println("Sum of numbers is: " + sumOfNumbers);
+        int average = sumOfNumbers/100;
+        System.out.println("Average number is: " + average);
     }
 }
