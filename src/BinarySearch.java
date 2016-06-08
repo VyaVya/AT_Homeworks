861693f8ce71f8f67967acbadeca5ef61974d3be
Vladyslav_Yakymenko <chameleon91@yandex.ru>
2016-06-08 13:40:54 +0300

    Bubble sort: created own method for sorting

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -4,32 +4,28 @@ import java.util.Arrays;
  * Created by Vlad on 6/1/2016.
  */
 public class BinarySearch {
-        /*public static boolean contains(int[] arr, int item) {
-            int index = Arrays.binarySearch(arr, item);
-            return index >= 0;
-        }*/
+    //int[] array = new int[]{1, 5, 6, 3, 4, 2, 7, 9, 8, 0};
 
-    int[] arr = { 5, 2, 17, 13, 12, 19, 7, 3, 9, 15 };
     public static void main(String[] args) {
-        /*Arrays.sort(myArray);
-          System.out.println(contains(myArray, 13));
-          System.out.println(contains(myArray, 25));*/
+        BinarySearch search = new BinarySearch();
+        search.bubbleSort(new int[]{1, 5, 6, 3, 4, 2, 7, 9, 8, 0});
+        System.out.println(search);
 
     }
 
-    public void bubbleSort(int[] arr) {
-        boolean swapped = true;
-        int j = 0;
-        int tmp;
-        while (swapped) {
-            swapped = false;
-            j++;
-            for (int i = 0; i < arr.length - j; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    tmp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = tmp;
-                    swapped = true;
+    public void bubbleSort(int[] array){
+        int temporaryValue;
+        boolean swap = true;
+        int a = 0;
+        while (swap) {
+            swap = false;
+            a++;
+            for (int i = 0; i < array.length - a; i++) {
+                if (array[i] > array[i + 1]) {
+                    temporaryValue = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temporaryValue;
+                    swap = true;
                 }
             }
         }
